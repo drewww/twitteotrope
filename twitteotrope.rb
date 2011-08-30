01b2da1a24709a254b3a6a3697d6e69e4dc353b1
Drew Harry <drew.harry@gmail.com>
2011-08-30 15:06:56 -0400

    Works, finally\! The old bits are all okay. Now to do better frames.

--- twitteotrope.rb
+++ twitteotrope.rb
@@ -162,56 +162,59 @@ class App
     
     def process_command
 
-      # begin
-      #   consumer_auth = YAML.load(IO.read('app_credentials.yml'))
-      # rescue
-      #   puts "Missing 'app_credentials.yml' file with consumer key and secret."
-      #   return
-      # end
-      # 
-      # 
-      # @consumer=OAuth::Consumer.new consumer_auth[:consumer_key], 
-      #                              consumer_auth[:consumer_secret], 
-      #                              {:site=>"http://api.twitter.com"}
-      # 
-      # begin
-      #   @auth = YAML.load(IO.read('user_credentials.yml'))
-      #   token_hash = {:token => auth["token"],
-      #                 :token_secret => auth["token_secret"]}
-      #   access_token = OAuth::AccessToken.from_hash(consumer, token_hash)
-      #   
-      #   # I know there's probably a way to merge keys in ruby, but I'm lazy
-      #   @auth[:consumer_key] = consumer_auth[:consumer_key]
-      #   @auth[:consumer_secret] = consumer_auth[:consumer_secret]
-      #   
-      # rescue
-      #   
-      #   # If the load fails, we need to start the oob auth process. 
-      #   # 1. Hit this url: https://api.twitter.com/oauth/request_token?oauth_callback=oob
-      #   # 2. Take the request_token and construct a new URL: http://api.twitter.com/oauth/authorize?oauth_token=request_token
-      #   # 3. Show that URL to the user and ask them to C&P it into a browser
-      #   # 4. Prompt for them to enter the PIN
-      #   # 5. POST (?) the PIN here: https://api.twitter.com/oauth/access_token 
-      #   
-      #   request_token = @consumer.get_request_token(:oauth_callback => "oob")
-      #   
-      #   puts "Load this URL in a browser: " + request_token.authorize_url
-      #   print "Enter PIN: "
-      #   pin = gets.chomp
-      #   
-      #   access_token = request_token.get_access_token(:oauth_verifier => pin)
-      #   
-      #   f = File.open("user_credentials.yml", 'w')
-      #   f.write(YAML.dump(
-      #       {:token => access_token.token,
-      #        :token_secret => access_token.secret}))
-      #   f.close
-      #   
-      #   # this is silly and gross but whatever. I'm a nub.
-      #   @auth = consumer_auth.clone
-      #   @auth[:token] = access_token.token
-      #   @auth[:token_secret] = access_token.secret
-      # end
+      begin
+        consumer_auth = YAML.load(IO.read('app_credentials.yml'))
+      rescue
+        puts "Missing 'app_credentials.yml' file with consumer key and secret."
+        return
+      end
+      
+
+      @consumer=OAuth::Consumer.new consumer_auth[:consumer_key], 
+                                         consumer_auth[:consumer_secret], 
+                                         {:site=>"http://api.twitter.com"}
+            
+      begin
+        puts "Loading user token and secret from file." if @options.verbose
+        
+        @auth = YAML.load(IO.read('user_credentials.yml'))
+        token_hash = {:token => @auth[:token],
+                      :token_secret => @auth[:token_secret]}
+
+        access_token = OAuth::AccessToken.from_hash(@consumer, token_hash)
+      
+        # I know there's probably a way to merge keys in ruby, but I'm lazy
+        @auth[:consumer_key] = consumer_auth[:consumer_key]
+        @auth[:consumer_secret] = consumer_auth[:consumer_secret]
+        
+      rescue
+        puts "Failed to load user token and secret, requesting authorization." if @options.verbose
+        # If the load fails, we need to start the oob auth process. 
+        # 1. Hit this url: https://api.twitter.com/oauth/request_token?oauth_callback=oob
+        # 2. Take the request_token and construct a new URL: http://api.twitter.com/oauth/authorize?oauth_token=request_token
+        # 3. Show that URL to the user and ask them to C&P it into a browser
+        # 4. Prompt for them to enter the PIN
+        # 5. POST (?) the PIN here: https://api.twitter.com/oauth/access_token 
+      
+        request_token = @consumer.get_request_token(:oauth_callback => "oob")
+      
+        puts "Load this URL in a browser: " + request_token.authorize_url
+        print "Enter PIN: "
+        pin = gets.chomp
+      
+        access_token = request_token.get_access_token(:oauth_verifier => pin)
+      
+        f = File.open("user_credentials.yml", 'w')
+        f.write(YAML.dump(
+            {:token => access_token.token,
+             :token_secret => access_token.secret}))
+        f.close
+      
+        # this is silly and gross but whatever. I'm a nub.
+        @auth = consumer_auth.clone
+        @auth[:token] = access_token.token
+        @auth[:token_secret] = access_token.secret
+      end
       
       
       
@@ -228,58 +231,40 @@ class App
       # retard.
       # puts "loaded twitter info for #{twitter_config["username"]}"
 
-      # frame_generator = ColorShiftFrameGenerator.new()
-      #       
-      #       current_time = Time.new.to_i
-      #       
-      #       puts "Generating frame for time: #{current_time}"
-      # 
-      #       image = frame_generator.get_frame(current_time)
-      #       
-      #       # bounce the produced image off a file
-      #       # we'll want a flag here eventually that bounce them off tmp so 
-      #       # they don't accumulate, but for now we can always store them.
-      #       
-      #       if image!=nil
-      #         # write the image to disk
-      #         image_file = "img/frame_#{current_time}.png"
-      #         image.write(image_file)
-      #       end
-      # 
-      #       if image_file==nil
-      #         puts "Frame generator returned a nil image"
-      #         return
-      #       end
-      # 
-      #       #Actually do the request and print out the response
-      #       if not @options.pretend
-      #         
-      #         update_profile_image(image_file)
-      #         
-      #         if @options.set_status
-      #           update_status_message(@options.status)
-      #         end
-      #         
-      #       end
+      frame_generator = ColorShiftFrameGenerator.new()
       
-      begin
-        oauth_config = YAML.load(IO.read('oauth.yml'))
-        #Make sure oauth_config contains symbol keys
-        oauth_config.replace(oauth_config.inject({}) {|h, (key,value)| h[key.to_sym] = value; h})
-      rescue
-        puts "You must have an oauth.yml file with your consumer and access info"
+      current_time = Time.new.to_i
+      
+      puts "Generating frame for time: #{current_time}"
+
+      image = frame_generator.get_frame(current_time)
+      
+      # bounce the produced image off a file
+      # we'll want a flag here eventually that bounce them off tmp so 
+      # they don't accumulate, but for now we can always store them.
+      
+      if image!=nil
+        # write the image to disk
+        image_file = "img/frame_#{current_time}.png"
+        image.write(image_file)
+      end
+
+      if image_file==nil
+        puts "Frame generator returned a nil image"
+        return
+      end
+
+      #Actually do the request and print out the response
+      if not @options.pretend
+        
+        update_profile_image(File.new(image_file))
+        
+        if @options.set_status
+          update_status_message(@options.status)
+        end
+        
       end
       
-      image_file = "img/frame_1314728598.png"
-      
-      url = URI.parse('http://api.twitter.com/1/account/update_profile_image.json')
-      Net::HTTP.new(url.host, url.port).start do |http| 
-        req = Net::HTTP::Post.new(url.request_uri)
-        add_multipart_data(req,:image=>image_file)
-        add_oauth(req,oauth_config)
-        res = http.request(req)
-        puts res.body
-      end
       
       #process_standard_input # [Optional]
     end
@@ -298,90 +283,83 @@ class App
     
     # in the app proper so it has access to the @options
     # member variable
-    def update_status_message(message)
-      
-      
-      if message == nil
-        message = "ding: #{Time.new.to_i}"
-      end
-
-      puts "Tweeting: " + message;
-
-      url = URI.parse('http://twitter.com/statuses/update.json')
-      Net::HTTP.new(url.host, url.port).start do |http| 
-        req = Net::HTTP::Post.new(url.request_uri)
-        req.basic_auth twitter_config["username"], twitter_config["password"]
-        req.set_form_data({'status'=>message})
-        res = http.request(req)
-
-        if @options.verbose
-          puts "Tweeting successful! Full response:"
-          puts res.body
-        end
-        
+    # def update_status_message(message)
+    #       
+    #       
+    #       if message == nil
+    #         message = "ding: #{Time.new.to_i}"
+    #       end
+    # 
+    #       puts "Tweeting: " + message;
+    # 
+    #       url = URI.parse('http://twitter.com/statuses/update.json')
+    #       Net::HTTP.new(url.host, url.port).start do |http| 
+    #         req = Net::HTTP::Post.new(url.request_uri)
+    #         req.basic_auth twitter_config["username"], twitter_config["password"]
+    #         req.set_form_data({'status'=>message})
+    #         res = http.request(req)
+    # 
+    #         if @options.verbose
+    #           puts "Tweeting successful! Full response:"
+    #           puts res.body
+    #         end
+    #         
+    #       end
+    #     end
+    
+    
+    def mime_type(file)
+      case 
+        when file =~ /\.jpg/ then 'image/jpg'
+        when file =~ /\.gif$/ then 'image/gif'
+        when file =~ /\.png$/ then 'image/png'
+        else 'application/octet-stream'
       end
     end
+
+    #Encodes the request as multipart
+    def add_multipart_data(req,params)
+      boundary = Time.now.to_i.to_s(16)
+      req["Content-Type"] = "multipart/form-data; boundary=#{boundary}"
+      body = ""
+      params.each do |key,value|
+        esc_key = CGI.escape(key.to_s)
+        body << "--#{boundary}#{CRLF}"
+        if value.respond_to?(:read)
+          body << "Content-Disposition: form-data; name=\"#{esc_key}\"; filename=\"#{File.basename(value.path)}\"#{CRLF}"
+          body << "Content-Type: #{mime_type(value.path)}#{CRLF*2}"
+          body << value.read
+        else
+          body << "Content-Disposition: form-data; name=\"#{esc_key}\"#{CRLF*2}#{value}"
+        end
+        body << CRLF
+      end
+      body << "--#{boundary}--#{CRLF*2}"
+      req.body = body
+      req["Content-Length"] = req.body.size
+    end
+
+    #Uses the OAuth gem to add the signed Authorization header
+    def add_oauth(req)
+      access_token = OAuth::AccessToken.new(@consumer,@auth[:token],@auth[:token_secret])
+      @consumer.sign!(req,access_token)
+    end
+
+    def update_profile_image(image_file)
+      url = URI.parse('http://api.twitter.com/1/account/update_profile_image.json')
+      Net::HTTP.new(url.host, url.port).start do |http| 
+        req = Net::HTTP::Post.new(url.request_uri)
+        add_multipart_data(req,:image=>image_file)
+        add_oauth(req)
+        res = http.request(req)
+        puts res.body
+      end
+    end
+    
     
 end
 
 
-def mime_type(file)
-  case 
-    when file =~ /\.jpg/ then 'image/jpg'
-    when file =~ /\.gif$/ then 'image/gif'
-    when file =~ /\.png$/ then 'image/png'
-    else 'application/octet-stream'
-  end
-end
-
-#Encodes the request as multipart
-def add_multipart_data(req,params)
-  boundary = Time.now.to_i.to_s(16)
-  req["Content-Type"] = "multipart/form-data; boundary=#{boundary}"
-  body = ""
-  params.each do |key,value|
-    esc_key = CGI.escape(key.to_s)
-    body << "--#{boundary}#{CRLF}"
-    if value.respond_to?(:read)
-      body << "Content-Disposition: form-data; name=\"#{esc_key}\"; filename=\"#{File.basename(value.path)}\"#{CRLF}"
-      body << "Content-Type: #{mime_type(value.path)}#{CRLF*2}"
-      body << value.read
-    else
-      body << "Content-Disposition: form-data; name=\"#{esc_key}\"#{CRLF*2}#{value}"
-    end
-    body << CRLF
-  end
-  body << "--#{boundary}--#{CRLF*2}"
-  req.body = body
-  req["Content-Length"] = req.body.size
-end
-
-#Uses the OAuth gem to add the signed Authorization header
-def add_oauth(req,auth)
-
-  print auth
-  
-  consumer = OAuth::Consumer.new(
-    auth[:consumer_key],auth[:consumer_secret],{:site=>'http://api.twitter.com'}
-  )
-  access_token = OAuth::AccessToken.new(consumer,auth[:token],auth[:token_secret])
-  consumer.sign!(req,access_token)
-end
-
-def update_profile_image(image_file)
-  oauth_config = YAML.load(IO.read('oauth.yml'))
-  #Make sure oauth_config contains symbol keys
-  oauth_config.replace(oauth_config.inject({}) {|h, (key,value)| h[key.to_sym] = value; h})
-  
-  url = URI.parse('http://api.twitter.com/1/account/update_profile_image.json')
-  Net::HTTP.new(url.host, url.port).start do |http| 
-    req = Net::HTTP::Post.new(url.request_uri)
-    add_multipart_data(req,:image=>image_file)
-    add_oauth(req,oauth_config)
-    res = http.request(req)
-    puts res.body
-  end
-end
 
 # Create and run the application
 app = App.new(ARGV, STDIN)
